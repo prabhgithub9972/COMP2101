e929e8a0555420a3986adf5edbc1640880ef74ae
prabhgithub9972 <78553297+prabhgithub9972@users.noreply.github.com>
2021-03-27 23:09:26 -0400

    lab 1

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # this script generates a report of the files on the system that have the setuid permission bit turned on
-# it is for the lab1 exercise
+# it is for the lab1 exercse
 # it uses the find command to find files of the right type and with the right permissions, and an error redirect to
 # /dev/null so we do not get errors for inaccessible directories and files
 # the identified files are sorted by their owner
