49794679e6bd5e6641f65523bac9c0c806143de7
prabhgithub9972 <78553297+prabhgithub9972@users.noreply.github.com>
2021-04-21 21:01:33 -0400

    lab5

--- Powershell/lab5.ps1
+++ Powershell/lab5.ps1
@@ -1,55 +1,41 @@
-param (
-        [switch]$System,
+param ( [switch]$System, 
         [switch]$Disks,
         [switch]$Network
 )
 
-if ( $System -eq $false -and $Disks -eq $false -and $Network -eq $false ) {
-        systemReport.ps1
-}
-else {
-        Write-Output ""
-        Write-Output "System Information Report"
-        Write-Output "--------------------------"
-        Write-Output "
-        "
-        if ( $System -eq $true ) {
-    
-            Write-Output "Processor"
-            Write-Output "----------------------"
-            (Get-ProcessorInfo | Out-String).Trim()
-            Write-Output "
-            "    
-            Write-Output "Operating System"
-            Write-Output "----------------------"
-            (Get-SysInfo | Out-String).Trim()
-            Write-Output "
-    
-            "
-            Write-Output "Memory"
-            Write-Output "----------------------"
-            (Get-RAM | Out-String).Trim()
-            Write-Output "
-            "
-            Write-Output "Video Card"
-            Write-Output "----------------------"
-            (Get-VideoCard | Out-String).Trim()
-            Write-Output ""
-        }
+if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+        "SYSTEM INFO "
+        "HARDWARE INFO :  "
+        hardware_info | fl
+        "OS INFO : "
+        os_info | fl
+        "CPU INFO : "
+        processor_info | fl
+        "RAM INFO : "
+        memory_info| ft -AutoSize
+        "DISK INFO : "
+        disk_info | ft -AutoSize
+        "NET INFO: "
+        net_info | fl
+    } 
+else { 
+    if ($System) {
+        "HARDWARE INFO : "
+        hardware_info | fl
+        "OS INFO : "
+        os_info | fl
+        "CPU INFO : "
+        processor_info | fl
+        "RAM INFO : "
+        memory_info| ft -AutoSize
+    }
+    if ($Disks) {
+        "DISK INFO : "
+        disk_info | ft -AutoSize
+    }
+    if ($Network) {
+        "NETWORK INFO :"
+        net_info | fl
+    }
 
-        if ( $Disks -eq $true ) {
-    
-            Write-Output "Physical Disk Drives"
-            Write-Output "----------------------"
-            (Get-Summary | Out-String).Trim()
-            Write-Output ""
-        }
-
-        if ( $Network -eq $true ) {
-
-            Write-Output "Network Adapter"
-            Write-Output "----------------------"
-            (Get-IPConfig | Out-String).Trim()
-            Write-Output ""
-        }
 }
