8e250db00519cf10ab3edbd53c78d1b274683084
prabhgithub9972 <78553297+prabhgithub9972@users.noreply.github.com>
2021-04-21 20:33:19 -0400

    lab5

--- Powershell/lab5.ps1
+++ Powershell/lab5.ps1
@@ -1,169 +1,55 @@
-function welcome {
-write-output "Welcome to planet $env:computername Overlord $env:username"
-$now = get-date -format 'HH:MM tt on dddd'
-write-output "It's $now."
+param (
+        [switch]$System,
+        [switch]$Disks,
+        [switch]$Network
+)
+
+if ( $System -eq $false -and $Disks -eq $false -and $Network -eq $false ) {
+        systemReport.ps1
 }
+else {
+        Write-Output ""
+        Write-Output "System Information Report"
+        Write-Output "--------------------------"
+        Write-Output "
+        "
+        if ( $System -eq $true ) {
+    
+            Write-Output "Processor"
+            Write-Output "----------------------"
+            (Get-ProcessorInfo | Out-String).Trim()
+            Write-Output "
+            "    
+            Write-Output "Operating System"
+            Write-Output "----------------------"
+            (Get-SysInfo | Out-String).Trim()
+            Write-Output "
+    
+            "
+            Write-Output "Memory"
+            Write-Output "----------------------"
+            (Get-RAM | Out-String).Trim()
+            Write-Output "
+            "
+            Write-Output "Video Card"
+            Write-Output "----------------------"
+            (Get-VideoCard | Out-String).Trim()
+            Write-Output ""
+        }
 
+        if ( $Disks -eq $true ) {
+    
+            Write-Output "Physical Disk Drives"
+            Write-Output "----------------------"
+            (Get-Summary | Out-String).Trim()
+            Write-Output ""
+        }
 
-function get-cpuinfo {
-get-ciminstance cim_processor | 
-select Name, MaxClockSpeed, CurrentClockSpeed, NumberOfCores, Manufacturer
-}
+        if ( $Network -eq $true ) {
 
-
-function get-mydisks {
-
-(new-object -typename psobject -property @{
- Model=(get-disk).Model;
- Manufacturer=(get-disk).Manufacturer;
- SerialNumber=(get-disk).SerialNumber;
- FirmwareVersion=(get-disk).FirmwareVersion;
- Size=(get-disk).Size;
-
- }) | ft
-
-}
-
-function get-sysInfo {
-
-"**System Information**"
-
-
-
-    Get-WmiObject win32_computersystem
-
-}
-
-function get-osInfo {
-
-"**OS Information**
-"
-
-    Get-WmiObject -Class win32_operatingsystem |
-        foreach {
-            New-Object -TypeName psobject -Property @{
-                OSName = $_.Name
-                Version = $_.Version
-                }
-        } 
-        ft -AutoSize OsName,
-                     Version
-
-}
-
-function get-proInfo {
-
-"**Processor Information ** 
-"
-
-
-
-    Get-WmiObject -Class win32_processor |
-        foreach {
-            New-Object -TypeName psobject -Property @{
-                Speed = $_.MaxClockSpeed
-                NumberOfCores = $_.NumberOfCores
-                L1CacheSize = "data unavailable"
-                L2CacheSize = "data unavailable"
-                L3CacheSize = $_.L3CacheSize
-                }
-        } 
-        ft -AutoSize Speed,
-                     NumberOfCores,
-                     L1CacheSize,
-                     L2CacheSize,
-                     L3CacheSize
-
-                     "
-                     "
-
-}
-
-function get-ramInfo {
-
-"**RAM information**"
-
-
-    $totalCapacity = 0
-
-    Get-WmiObject -Class win32_physicalmemory |
-        foreach {
-            New-Object -TypeName psobject -Property @{
-                Vendor = $_.Manufacturer
-                Model = "data unavailable"
-                "Size(MB)" = $_.capacity/1mb
-                Bank = $_.banklabel
-                Slot = $_.devicelocator
-                }
-            $totalCapacity += $_.capacity/1mb
-        } |
-            ft -auto Vendor,
-                      Model,
-                     "Size(MB)",
-                      Bank,
-                      Slot
-
-      "Total RAM: ${totalCapacity}MB
-      
-      "
-
-}
-
-function get-diskInfo {
-
-"**Disk Information**
-"
-
-
-
-        $diskdrives = Get-CIMInstance CIM_diskdrive
-
-  foreach ($disk in $diskdrives) {
-      $partitions = $disk|get-cimassociatedinstance -resultclassname CIM_diskpartition
-      foreach ($partition in $partitions) {
-            $logicaldisks = $partition | get-cimassociatedinstance -resultclassname CIM_logicaldisk
-            foreach ($logicaldisk in $logicaldisks) {
-                     new-object -typename psobject -property @{Vendor=$disk.Manufacturer
-                                                               Model=$disk.Model
-                                                               "SizeFree(GB)"=$logicaldisks.FreeSpace / 1gb -as [int]
-                                                               "SpaceFree(%)"=($logicaldisk.FreeSpace/$logicaldisk.size)*100 -as [int]
-                                                               "Size(GB)"=$logicaldisk.size / 1gb -as [int]
-                                                               } | ft
-           }
-      }
-  }
-
-}
-
-
-function get-adaptInfo {
-
-  "**Network Adapters** "
-
-
-
-  get-ciminstance win32_networkadapterconfiguration |
-     Where-Object ipenabled -eq True |
-     Select-Object Discription, index, IPAddress, subnetmask, dnsdomain, dnsserver
-        ft
-
-}
-
-function get-videoInfo {
-
-"**Video Card Information**
-"
-
-
-
-    Get-WmiObject -Class win32_videocontroller |
-        foreach {
-            New-Object -TypeName psobject -Property @{
-                Vendor = $_.Name
-                Description = $_.Description
-                CurrentScreenResolution = ($_.CurrentHorizontalResolution) * ($_.CurrentVerticalResolution)
-                
-                }
-        } 
-
+            Write-Output "Network Adapter"
+            Write-Output "----------------------"
+            (Get-IPConfig | Out-String).Trim()
+            Write-Output ""
+        }
 }
