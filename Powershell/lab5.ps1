f4751b69a33d85277952b83d54f17ef6cd30fc56
prabhgithub9972 <78553297+prabhgithub9972@users.noreply.github.com>
2021-04-21 21:14:12 -0400

    lab5

--- Powershell/lab5.ps1
+++ Powershell/lab5.ps1
@@ -1,41 +1,44 @@
-param ( [switch]$System, 
-        [switch]$Disks,
-        [switch]$Network
-)
+#-System displays the cpu, OS, RAM, Video reports ony
+	#-Disks displays the disks report only
+	#-Network displays the network report only
+	#$args
+	param ( [switch]$System, 
+	        [switch]$Disks,
+	        [switch]$Network
+	)
+	
 
-if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
-        "SYSTEM INFO "
-        "HARDWARE INFO :  "
-        hardware_info | fl
-        "OS INFO : "
-        os_info | fl
-        "CPU INFO : "
-        processor_info | fl
-        "RAM INFO : "
-        memory_info| ft -AutoSize
-        "DISK INFO : "
-        disk_info | ft -AutoSize
-        "NET INFO: "
-        net_info | fl
-    } 
-else { 
-    if ($System) {
-        "HARDWARE INFO : "
-        hardware_info | fl
-        "OS INFO : "
-        os_info | fl
-        "CPU INFO : "
-        processor_info | fl
-        "RAM INFO : "
-        memory_info| ft -AutoSize
-    }
-    if ($Disks) {
-        "DISK INFO : "
-        disk_info | ft -AutoSize
-    }
-    if ($Network) {
-        "NETWORK INFO :"
-        net_info | fl
-    }
+	
 
-}
+	
+
+	if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+	    full_report.ps1
+	} else {
+	    "`nYour System Information Report"
+	    if ($System) {
+	        "`n`nProcessor:"
+	        "==================================="
+	        (CPU_info | fl | out-string).trim()
+	        "`n`nOperating system:"
+	        "==================================="
+	        (operating_system_info | fl | out-string).trim()
+	        "`n`nMemory:"
+	        "==================================="
+	        (memory_info | ft -AutoSize | out-string).trim()
+	        "`n`nVideo Card:"
+	        "==================================="
+	        (video_card | fl | out-string).trim()
+	    }
+	    if ($Disks) {
+	        "`n`nDisk:"
+	        "==================================="
+	        (disk_info | ft -AutoSize Drive,Vendor,Model,“Size(GB)”,"Free Space(GB)","Space Uasge(%)" | out-string).trim()
+	    }
+	    if ($Network) {
+	        "`n`nNetwork Adapter:"
+	        "==================================="
+	        (Network_info | ft -AutoSize | out-string).trim()
+	    }
+	}
+
